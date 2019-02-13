d3aa21774ef117a40aa547ebc96c4834bc0ed883
nkarij <5608562@rts-365.dk>
2019-02-13 13:01:20 +0100

    update

--- 01/dist/weather.js
+++ 01/dist/weather.js
@@ -6,9 +6,11 @@ let weatherChangeImageFolder = {
     Snow:"IMG/weather/snow.png"
 }
 
-let apiDatabaseURL = "http://api.openweathermap.org/data/2.5/weather?";
-let weatherLocation = "q=Copenhagen,dk";
-let apiKeyId = "&APPID=8acbc6e8a7dc23dc79a92890a707d33b";
+const apiDatabaseURL = "http://api.openweathermap.org/data/2.5/weather?";
+const weatherLocation = "id=2618425&units=metric";
+const apiKeyId = "&APPID=8acbc6e8a7dc23dc79a92890a707d33b";
+const weatherImageElement = document.querySelector(".weather-image");
+const temperatureDegreesElement = document.querySelector(".forecast__grader"); 
 
 fetch(apiDatabaseURL + weatherLocation + apiKeyId)
 .then((response)=>{
@@ -18,29 +20,50 @@ fetch(apiDatabaseURL + weatherLocation + apiKeyId)
 .then((weather)=>{
 
     let currentWeather = weather.weather[0].main;
+    // console.log(currentWeather);
+
     getWeatherInfo(currentWeather);
     matchWeatherAndImage();
 
+    // den her return er vigtig (selvom jeg ikke helt forstår hvorfor...)
     function getWeatherInfo(weather){
-        console.log(weather);
-        console.log(typeof(weather));
+        // console.log(weather);
+        // console.log(typeof(weather));
         return currentWeather;
     }
-    
+
     function matchWeatherAndImage(){
         if(currentWeather == "Clouds"){
-            //  her skal jeg lave noget kode som kan indsætte korrekt billede
-            getWeatherPicture(Clouds);
-            console.log("hej");
+            getWeatherPicture("Clouds");
+            setCorrectTemperature();
         }
+        if(currentWeather == "Clear"){
+            getWeatherPicture("Sun");
+            setCorrectTemperature();
+        }
+        if(currentWeather == "Rain"){
+            getWeatherPicture("Rain");
+            setCorrectTemperature();
+        }
+        if(currentWeather == "Snow"){
+            getWeatherPicture("Snow");
+            setCorrectTemperature();
+        }
+
     }
-    setTimeout(matchWeatherAndPicture, 2000);
 
-    let weatherImageElement = document.querySelector(".weather-image");
+    function getWeatherPicture(keystring){
+        // let imageSource = weatherChangeImageFolder[keystring];
+        // console.log(imageSource);
+        weatherImageElement.setAttribute("src", weatherChangeImageFolder[keystring]);
 
-    let  = function getWeatherPicture(keystring){
-        weatherImageElement.setAttribute("src", imageFolder[keystring]);
-        weatherImageElement.src
-    }(weatherChangeImageFolder);
+    }
+
+    function setCorrectTemperature(){
+        let currentTemperatur = weather.main.temp;
+        // console.log(currentTemperatur);
+        temperatureDegreesElement.innerHTML = currentTemperatur;
+    }
+    
 
 });
